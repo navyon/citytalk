b66ff42b33167a0e2fa2a4315fb517f094aecd2b
navyon <net_systems@live.com>
2013-11-04 14:06:02 +0100

    Latest version with Splash Screen

--- src/com/example/CityTalk/MyActivity.java
+++ src/com/example/CityTalk/MyActivity.java
@@ -1,9 +1,17 @@
 package com.example.CityTalk;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
 
 public class MyActivity extends Activity {
     /**
@@ -14,8 +22,13 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-
-
+        Button btntakephoto =(Button)findViewById(R.id.btnTakePhoto);
+        Button btnuploadphoto =(Button)findViewById(R.id.btn_UploadPhoto);
+        Button btncontinue =(Button)findViewById(R.id.btn_Continue);
+        Typeface typeFace= Typeface.createFromAsset(getAssets(), "fonts/RobotoBold.ttf");
+        btntakephoto.setTypeface(typeFace);
+        btnuploadphoto.setTypeface(typeFace);
+        btncontinue.setTypeface(typeFace);
     }
 
 }
