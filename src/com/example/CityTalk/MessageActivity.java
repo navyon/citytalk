cebb779c6467d46ac98f922ac5393553bd797fff
navyon <net_systems@live.com>
2013-12-14 13:32:28 +0100

    Fixed the
    Fixed Max lines in Text preview

--- src/com/example/CityTalk/MessageActivity.java
+++ src/com/example/CityTalk/MessageActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -21,14 +22,15 @@ import java.io.ByteArrayOutputStream;
  // The Message Activity
 public class MessageActivity extends Activity {
      Intent intent;
+      EditText  txtView_msg;
 
        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message);
-          /* final EditText txtview = (EditText) findViewById(R.id.txtView_msg);
+           txtView_msg = (EditText) findViewById(R.id.txtView_msg);
            final Button btnpreview = (Button) findViewById(R.id.btnpreview);
-*/
+
            // The Image from the previous Activity is decoded and stored as a bitMap, so it can be sent on to the Next
            intent = new Intent(MessageActivity.this, Preview.class);
            if(getIntent().hasExtra("theimage")){
@@ -40,17 +42,31 @@ public class MessageActivity extends Activity {
 
              }
            }
+
            findViewById(R.id.btnpreview).setOnClickListener(new View.OnClickListener() {
                public void onClick(View v) {
 
 
-
                    startActivity(intent);
-               }});
+               }
+           });
+           // This on key listener gets the number of lines if larger than 4 it ignore the Enter Key Press
+           txtView_msg.setOnKeyListener(new View.OnKeyListener() {
 
+               @Override
+               public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                   if (keyCode == KeyEvent.KEYCODE_ENTER  && event.getAction() == KeyEvent.ACTION_DOWN) {
+
+                       if ( ((EditText)v).getLineCount() >= 4 )
+                           return true;
+                   }
+
+                   return false;
+               }
+           });
 
     }
-
 
 
 
