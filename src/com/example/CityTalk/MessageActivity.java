746fdb6879d28d011d622a52945bd9319eda0437
navyon <net_systems@live.com>
2013-12-19 18:34:46 +0100

    latest version
    added the check for no message feature

--- src/com/example/CityTalk/MessageActivity.java
+++ src/com/example/CityTalk/MessageActivity.java
@@ -10,28 +10,46 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.*;
 
 import java.io.ByteArrayOutputStream;
 
  // The Message Activity
 public class MessageActivity extends Activity {
      Intent intent;
-      EditText  txtView_msg;
-      String msg = null;
+     private EditText  txtView_msg;
+      //String msg = null;
        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message);
-           txtView_msg = (EditText) findViewById(R.id.txtView_msg);
-           final Button btnpreview = (Button) findViewById(R.id.btnpreview);
 
+           txtView_msg = (EditText) findViewById(R.id.txtView_msg);
+           Button btnPrev = (Button) findViewById(R.id.btnpreview);
+
+           btnPrev.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+
+                   String defaultmsg = "Voer hier direct jouw bericht in.";
+
+                   String msg = (String) txtView_msg.getText().toString();
+
+                   if (!msg.isEmpty()) {
+                       intent.putExtra("msg", msg);
+                       startActivity(intent);
+
+                   } else {
+                       Toast.makeText(getApplicationContext(), "Het toevoegen van een bericht is verplicht.", Toast.LENGTH_LONG).show();
+                   }
+
+
+               }
+           });
 
 
            // The Image from the previous Activity is decoded and stored as a bitMap, so it can be sent on to the Next
@@ -46,24 +64,8 @@ public class MessageActivity extends Activity {
              }
            }
 
-           findViewById(R.id.btnpreview).setOnClickListener(new View.OnClickListener() {
-               public void onClick(View v) {
 
-                   String  defaultmsg = "Voer hier direct jouw bericht in.";
-                   msg = txtView_msg.getText().toString();
-                   if(!defaultmsg.equalsIgnoreCase(msg))
-                   {
-                       intent.putExtra("msg",msg);
-                       startActivity(intent);
-                   }
-                   else
-                   {
-                       Toast.makeText(getApplicationContext(),"Please add your Message", Toast.LENGTH_SHORT).show();
 
-                   }
-
-               }
-           });
            // This on key listener gets the number of lines if larger than 4 it ignore the Enter Key Press
            txtView_msg.setOnKeyListener(new View.OnKeyListener() {
 
