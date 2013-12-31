b5f7803f5051214a9baabece153e6a994dba2f5f
navyon <net_systems@live.com>
2013-12-31 15:05:13 +0100

    added like button event
    this event just opens a web page to the beeld van denhaag fb page

--- src/com/example/CityTalk/FinalActivity.java
+++ src/com/example/CityTalk/FinalActivity.java
@@ -4,6 +4,7 @@ package com.example.CityTalk;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.FeatureInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,13 @@ public class FinalActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landingpage);
 
+        findViewById(R.id.btnlike).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent open = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/BeeldVanDenHaag"));
+                startActivity(open);
+            }
+        });
     }
 
 
